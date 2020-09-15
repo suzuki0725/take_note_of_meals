433da523fddb86ea98dae1d6be5445b094213955
suzuki0725 <los_campesinos1130@yahoo.co.jp>
2020-09-15 09:52:26 +0900

    Create blogs_controller

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,29 @@
+class BlogsController < ApplicationController
+  def index
+    @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def create
+    Blog.create(blog_parameter)
+    redirect_to blogs_path
+  end
+
+
+
+  private
+
+  def blog_parameter
+    parameter.require(:blog).permit(:title, :content, :start_time)
+  end
+
+
+
+end
